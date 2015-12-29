bafc7208db9a2fed0ef88c3f8cf1bd9419e17986
gitamar <gdgamarnath@gmail.com>
2015-12-29 17:18:44 +0530

    First and last

---
+++ chir/craftedbygc.com/app.js
@@ -0,0 +1,20 @@
+var sampleApp = angular.module('sampleApp', []);
+
+sampleApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+		when('/newyear/:orderId', {
+			templateUrl: 'templates/show_order.html',
+			controller: 'ShowOrderController'
+		});
+		
+	}
+]);
+
+
+sampleApp.controller('ShowOrderController', function($scope, $routeParams) {
+
+
+	$scope.order_id = $routeParams.orderId;
+
+});
